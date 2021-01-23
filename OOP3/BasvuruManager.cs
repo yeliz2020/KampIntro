7988e041cd1f0b0ff2f25228b65c65bbf2e95ef2
Yeliz <yeliz.isler@gmail.com>
2021-01-23 23:53:39 +0300

    23.01.2021

---
+++ OOP3/BasvuruManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    class BasvuruManager
+    {
+
+        //Method Injection
+        public void BasvuruYap(IKrediManager krediManager, ILoggerService loggerService)
+        {
+            //kjfkfjefj
+            //lflkfekfj
+            krediManager.Hesapla();
+            loggerService.Log();
+        }
+
+        public void KrediOnBilgilendirmesiYap(List<IKrediManager> krediler)
+        {
+            foreach (var kredi in krediler)
+            {
+                kredi.Hesapla();
+            }
+        }
+    }
+}
---
+++ Oop2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Oop2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Engin";
+            musteri1.Soyadi = "Demiroğ";
+            musteri1.TcNo = "12345678910";
+            musteri1.Id = 1;
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "54321";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "324848475";
+
+            Musteri musteri3 = new Musteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+
+
+
+        }
+    }
+}
